7cd4b4f16fa97bf220d8e1bb4e5aeabfc6c1802b
Daniel Quaidoo <daniel.quaidoo@gmail.com>
2024-11-15 08:48:05 +0000

    Altered backend structure added test example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { UserRoleModule } from './user_role/user_role.module';
-import { RolesModule } from './roles/roles.module';
-import { FilesModule } from './files/files.module';
-import { FuelStationsModule } from './fuel_stations/fuel_stations.module';
-import { FuelStationTransactionModule } from './fuel_station_transaction/fuel_station_transaction.module';
-import { RolePermissionModule } from './role_permission/role_permission.module';
-import { FuelStationServiceModule } from './fuel_station_service/fuel_station_service.module';
-import { ServicesModule } from './services/services.module';
-import { PermissionsModule } from './permissions/permissions.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
 import databaseConfig from './config/database.config';
-
-// imports: [UsersModule, UserRoleModule, RolesModule, FilesModule, FuelStationsModule, FuelStationTransactionModule, RolePermissionModule, FuelStationServiceModule, ServicesModule, PermissionsModule],
 
 @Module({
   imports: [
@@ -40,6 +29,9 @@ import databaseConfig from './config/database.config';
       }),
       inject: [ConfigService],
     }),
+
+    // Feature modules
+    UsersModule, // Register the UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
